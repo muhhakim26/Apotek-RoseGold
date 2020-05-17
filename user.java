438d9a8f76cd04d8e8557b6c49708363df1542b1
AnnisaJufeAryani <65056172+AnnisaJufeAryani@users.noreply.github.com>
2020-05-18 00:21:22 +0700

    Add files via upload

---
+++ user.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tubespbo.program;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Muhammad Abdul Hakim
+ */
+public class user {
+    
+     private static Connection MySQLConfig;
+    
+    public static Connection configDB()throws SQLException{
+        try{
+            String url = "jdbc:mysql://localhost:3306/apotek_rosegold";
+            String user = "root";
+            String pass = "";
+            
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            MySQLConfig = DriverManager.getConnection(url, user, pass);
+            
+        }catch(SQLException e){
+            System.out.println("Gagal Koneksi ke Database" + e.getMessage());
+        }
+        return MySQLConfig;
+            
+    }
+    
+}
